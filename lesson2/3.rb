e4dffb7e5114831f4067335c6a3a9a444b3af191
ShilovK <shilovk@gmail.com>
2019-11-22 00:37:18 +0300

    done fibonachi
    This reverts commit 37cce388a1c97c48b9faa86370a1b932bcced587.

--- lesson2/3.rb
+++ lesson2/3.rb
@@ -1,14 +1,8 @@
 # 3. Заполнить массив числами фибоначчи до 100 
 
-def fibonachi(max_value)
+def fibonachi(number)
   fib = [0, 1]
-  n = 2
-  loop do
-    value = fib[n-1] + fib[n-2]
-    break if value > max_value
-    fib[n] = value
-    n += 1
-  end
+  (2..number).each { |n| fib[n] = fib[n-1] + fib[n-2] }
   fib
 end
 
