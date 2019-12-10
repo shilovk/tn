c7f1e8e71284226faf861fb1b04713abb87c226f
ShilovK <shilovk@gmail.com>
2019-12-10 16:39:14 +0300

    fix

--- lesson11/deck.rb
+++ lesson11/deck.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
-require_relative 'modules/cards_generate'
+require_relative 'modules/generate'
 require_relative 'card'
 
 # Deck
 class Deck
-  attr_accessor :cards
+  attr_reader :cards
 
   def initialize
-    @cards = []
-    CardsGenerate.cards_values.each do |(card, point)|
-      @cards << Card.new(card, point)
+    @cards = Generate.cards_values.each_with_object([]) do |(name, point), memo|
+      memo << Card.new(name, point)
     end
   end
 
   def take_card
-    card = cards.sample
-    cards.delete(card)
-    card
+    cards.delete(cards.sample)
   end
+
+  protected
+
+  attr_writer :cards
 end
--- lesson11/modules/generate.rb
+++ lesson11/modules/generate.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# CardsGenerate
-module CardsGenerate
+# Generate
+module Generate
   TYPES =  %w[♠ ♥ ♣ ♦].freeze
   NAMES =  %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
   VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [1, 10]].freeze
